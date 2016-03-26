ca640772b462cb9e5fa5e45d14971e89eefc6b62
André Gimenez Faria <andfaria13@gmail.com>
2016-03-26 15:08:29 -0300

    xcode project and pods setup

---
+++ AcronymsExplorer/AcronymsExplorer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AcronymsExplorer
+//
+//  Created by André Gimenez Faria on 26/03/16.
+//  Copyright © 2016 AGF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AcronymsExplorer/AcronymsExplorer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AcronymsExplorer
+//
+//  Created by André Gimenez Faria on 26/03/16.
+//  Copyright © 2016 AGF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
